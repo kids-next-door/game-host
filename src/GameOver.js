3489943e6a6d100def529636a75a68d3c9540223
Jonah Witcig <jwitcig@gmail.com>
2019-11-22 08:00:16 -0600

    Fixed completion time sorting.

--- src/GameOver.js
+++ src/GameOver.js
@@ -1,59 +1,59 @@
-import React from "react";
-import moment from "moment";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
+import React from "react"
+import moment from "moment"
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
 
-import PostGameLeaderboard from "./PostGameLeaderboard";
+import PostGameLeaderboard from "./PostGameLeaderboard"
 
-import { useObject } from "react-firebase-hooks/database";
+import { useObject } from "react-firebase-hooks/database"
 
-import { useParams, Redirect, Link } from "react-router-dom";
+import { useParams, Redirect, Link } from "react-router-dom"
 
-const firebase = require("./config-firebase");
+const firebase = require("./config-firebase")
 
 const restartGame = gameKey =>
   firebase
     .database()
     .ref(`games/${gameKey}/status`)
-    .set("ready");
+    .set("ready")
 
 const backToLobby = gameKey =>
   firebase
     .database()
     .ref(`games/${gameKey}/status`)
-    .set("closed");
+    .set("closed")
 
-const NoGameFound = () => <h1>No Game Found!</h1>;
+const NoGameFound = () => <h1>No Game Found!</h1>
 
 const State = props => {
-  const params = useParams();
-  const gameID = params.id;
+  const params = useParams()
+  const gameID = params.id
 
-  const gameRef = firebase.database().ref(`games/${gameID}`);
+  const gameRef = firebase.database().ref(`games/${gameID}`)
 
-  const [snapshot, loading, error] = useObject(gameRef);
+  const [snapshot, loading, error] = useObject(gameRef)
   if (!snapshot) {
-    return <NoGameFound />;
+    return <NoGameFound />
   }
 
-  const game = snapshot.val();
+  const game = snapshot.val()
 
   if (game.status === "in-progress") {
-    return <Redirect to={`/game/${snapshot.key}`} />;
+    return <Redirect to={`/game/${snapshot.key}`} />
   } else if (game.status === "closed") {
-    return <Redirect to="/new-game" />;
+    return <Redirect to="/new-game" />
   }
 
-  const startTime = moment(game.started);
+  const startTime = moment(game.started)
 
   const players = Object.keys(game.player_state)
     .map((playerID, index) => {
-      const finishTime = moment(game.player_state[playerID].finished);
-      const rawDuration = moment.duration(finishTime.diff(startTime));
+      const finishTime = moment(game.player_state[playerID].finished)
+      const rawDuration = moment.duration(finishTime.diff(startTime))
       const duration = moment.utc(+rawDuration);
 
-      const minutes = parseInt(duration.format("m"));
-      const seconds = parseInt(duration.format("s"));
+      const minutes = parseInt(duration.format("m"))
+      const seconds = parseInt(duration.format("s"))
 
       return {
         id: playerID,
@@ -64,13 +64,13 @@ const State = props => {
       ...player,
       name: game.connected_players[player.id].name,
       duration: `${Math.floor(player.duration / 60)}m ${player.duration % 60}s`
-    }));
+    }))
 
-  const ordered = [...players].sort((a, b) => a.duration < b.duration);
-  const ranked = ordered.map((x, index) => ({ ...x, rank: index + 1 }));
+  const ordered = [...players].sort((a, b) => a.duration < b.duration ? -1 : 1)
+  const ranked = ordered.map((x, index) => ({ ...x, rank: index + 1 }))
 
-  return props.render({ game, players: ranked, gameKey: snapshot.key });
-};
+  return props.render({ game, players: ranked, gameKey: snapshot.key })
+}
 
 const Render = ({ game, players, gameKey }) => (
   <Box display="flex" p={1}>
@@ -107,4 +107,4 @@ const Render = ({ game, players, gameKey }) => (
   </Box>
 );
 
-export default props => <State render={p => <Render {...p} />} />;
+export default props => <State render={p => <Render {...p} />} />
